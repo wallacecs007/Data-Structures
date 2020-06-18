887660059ad5783a0f908c8385bf1dd20706a6d7
Chase Wallace <wallacecs007@gmail.com>
2020-06-17 19:43:41 -0600

    Queue is finished

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,45 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
 class Queue:
     def __init__(self):
         self.size = 0
         # self.storage = ?
-    
+        self.head = None
+        self.tail = None
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def dequeue(self):
-        pass
+        if not self.head:
+            return None
+
+        if self.head.next_node is None:
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
+
+        head_value = self.head.value
+        self.head = self.head.next_node
+        return head_value
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,43 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+
 class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.head = None
+        self.tail = None
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+
+        new_node = Node(value)
+
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
 
     def pop(self):
-        pass
+
+        if not self.head:
+            return None
+
+        if self.head.next_node is None:
+            temp_value = self.head.value
+            self.head = None
+            self.tail = None
+            return temp_value
+
+        tail_value = self.head.value
+        self.head = self.head.next_node
+        return head_value
